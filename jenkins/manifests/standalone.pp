2c3f51a812687943e78188095c4ff46bfeb19aa0
Eddú Meléndez <eddu.melendez@gmail.com>
2015-03-10 02:55:39 -0500

    adding more plugins

--- jenkins/manifests/standalone.pp
+++ jenkins/manifests/standalone.pp
@@ -2,17 +2,37 @@ node 'standalone' {
   include jenkins
 
   jenkins::plugin {
+    "artifactdeployer" : ;
     "blink1" : ;
     "build-name-setter" : ;
     "build-pipeline-plugin" : ;
     "build-with-parameters" : ;
     "disk-usage" : ;
+    "durable-task" : ;
+    "parameterized-trigger" : ;
+    "mailer" :
+      version => 'latest';
+    "jquery" : ;
+    "scm-api" : ;
+    "matrix-project" : ;
+    "ssh-credentials" : ;
+    "credentials" :
+      version => 'latest';
     "git" : ;
+    "github-api" : ;
+    "multiple-scms" : ;
     "git-client" : ;
+    "git-server" : ;
+    "subversion" :
+      version => 'latest';
+    "mapdb-api" : ;
     "github" : ;
     "gitlab-plugin" : ;
     "gradle" : ;
+    "maven-plugin" :
+      version => 'latest';
     "sonar" : ;
+    "view-job-filters" : ;
     "workflow-aggregator" : ;
     "workflow-api" : ;
     "workflow-basic-steps" : ;
